80e31897007747760d479dc31f7a04102684ca45
bifeissound <107683185+bifeissound@users.noreply.github.com>
2022-06-17 12:58:31 +0100

    index added

---
+++ data.js
@@ -0,0 +1,322 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-hall1",
+      "name": "Hall1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1800,
+      "initialViewParameters": {
+        "yaw": 0.23506718728777187,
+        "pitch": 0.3248775009656626,
+        "fov": 1.2933824216075565
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.9028657582831787,
+          "pitch": 0.01669388917764536,
+          "rotation": 0,
+          "target": "3-sta"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.8671883747067035,
+          "pitch": -0.1045496004842228,
+          "title": "Music Department",
+          "text": "Access to Recording Studio A, Studio Lab B and DJ Studio C including DJ booths"
+        },
+        {
+          "yaw": 2.4609802697436702,
+          "pitch": -0.09892646452230025,
+          "title": "Canteen",
+          "text": "Hot &amp; Cold food served daily"
+        }
+      ]
+    },
+    {
+      "id": "1-bbox1",
+      "name": "BBox1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1800,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-bbox2",
+      "name": "BBOx2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1800,
+      "initialViewParameters": {
+        "yaw": 0.1939128793995284,
+        "pitch": 0.1327134698479746,
+        "fov": 1.2933824216075565
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.5921361366464133,
+          "pitch": -0.04766067108434591,
+          "rotation": 0,
+          "target": "6-hall2"
+        },
+        {
+          "yaw": -2.7299543427543878,
+          "pitch": -0.0006917720357400015,
+          "rotation": 0,
+          "target": "3-sta"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-sta",
+      "name": "StA",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1800,
+      "initialViewParameters": {
+        "yaw": 0,
+        "pitch": 0,
+        "fov": 1.2933824216075565
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.26249110038506096,
+          "pitch": -0.045004399821269914,
+          "rotation": 0,
+          "target": "2-bbox2"
+        },
+        {
+          "yaw": 2.8321281654511186,
+          "pitch": -0.02804024116263193,
+          "rotation": 0,
+          "target": "4-stb"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.22390456905115563,
+          "pitch": 0.11480191952610852,
+          "title": "Sound Desk",
+          "text": "BIFE have a fully equipped state of the art recording facility"
+        }
+      ]
+    },
+    {
+      "id": "4-stb",
+      "name": "StB",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1800,
+      "initialViewParameters": {
+        "yaw": -2.98401883290275,
+        "pitch": 0.14948972370635438,
+        "fov": 1.2933824216075565
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.716936371567698,
+          "pitch": 0.08212029025488121,
+          "rotation": 0,
+          "target": "5-stc"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-stc",
+      "name": "StC",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1800,
+      "initialViewParameters": {
+        "yaw": -3.040003598155847,
+        "pitch": 0.006142562159780596,
+        "fov": 1.2933824216075565
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.2269580689231656,
+          "pitch": -0.00951236066238792,
+          "rotation": 0,
+          "target": "3-sta"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0,
+          "pitch": 0,
+          "title": "DJ Booths Access",
+          "text": "6 fully equipped DJ booths"
+        }
+      ]
+    },
+    {
+      "id": "6-hall2",
+      "name": "Hall2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1800,
+      "initialViewParameters": {
+        "yaw": 0.3029109160419221,
+        "pitch": -0.21112565999768584,
+        "fov": 1.2933824216075565
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.7312666383214648,
+          "pitch": -0.015758823928539556,
+          "rotation": 0,
+          "target": "2-bbox2"
+        },
+        {
+          "yaw": 0.08360838794865799,
+          "pitch": -0.03404796585601488,
+          "rotation": 0,
+          "target": "0-hall1"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 2.914507019454554,
+          "pitch": -0.11333382882641452,
+          "title": "Stage",
+          "text": "BIFE students perform in dance, theatre and music performance shows throughout the year"
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": true
+  }
+};
